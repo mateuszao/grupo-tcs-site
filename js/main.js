43443b75afd7b22eb6f2ee2b2cd601fa6c0fc031
Mateus Garcia <mateusgarcia@id.uff.br>
2021-02-18 11:57:55 -0300

    Ajustando menu mobile

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $(window).on('scroll', function(){
     $('header').removeClass('menu-bg-scroll');
   }
 })
+
+const mainMenu = document.querySelector('.menu-nav ul');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
