6e1f61df63dffc7c278647436155788d29411814
Mateus Garcia <mateusgarcia@id.uff.br>
2021-03-01 15:56:04 -0300

    Criando as outras pages e adicionando no botao

--- js/main.js
+++ js/main.js
@@ -174,3 +174,32 @@ function initModal() {
 }
 
 initModal();
+
+
+function redirecionaPagina(){
+  
+  btnImpactar = document.querySelector('.card-body');
+  btnValorizar = document.querySelector('.card-body-dois');
+  btnInovar = document.querySelector('.card-body-tres');
+  btnEmpreender = document.querySelector('.card-body-quatro');
+
+  if(btnImpactar && btnValorizar && btnInovar && btnEmpreender){
+    btnImpactar.addEventListener('click', function() {
+      window.location.href = "file:///E:/Projetos/grupo-tcs-site/page-impactar.html";
+    });
+  
+    btnValorizar.addEventListener('click', function() {
+      window.location.href = "file:///E:/Projetos/grupo-tcs-site/page-valorizar.html";
+    });
+  
+    btnInovar.addEventListener('click', function() {
+      window.location.href = "file:///E:/Projetos/grupo-tcs-site/page-inovar.html";
+    });
+  
+    btnEmpreender.addEventListener('click', function() {
+      window.location.href = "file:///E:/Projetos/grupo-tcs-site/page-empreender.html";
+    });
+  }
+}
+
+redirecionaPagina();
