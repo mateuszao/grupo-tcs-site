c1914c81d3726088de74d4acc55c5459ec385eda
Mateus Garcia <mateusgarcia@id.uff.br>
2021-03-09 14:35:50 -0300

    Ajustando page-impactar e criando o carrosel das imagens

--- js/script-empresas.js
+++ js/script-empresas.js
@@ -1,8 +1,9 @@
 import menuMobile from './modules/menuMobile.js';
 import animaSite from './modules/animaSite.js';
 import initModal from './modules/initModal.js';
-
+import carrosel from './modules/carrosel.js';
 
 menuMobile();
 animaSite();
 initModal();
+carrosel();
